c3fd14169943831affa25d6ff7656df819fae202
Rypin <hhgfcswd@gmail.com>
2025-09-12 18:39:37 +0300

    refactor: обновил валидацию
    - в конфиге проверяется наличие поля role
    - телефон проверятся на то что это телефон
    - в эндпоинте проверяется наличие пользователя с принимаемым email
    - в catch теперь перебрасывается ошибка для миддлвары

--- src/config/validations/user.ts
+++ src/config/validations/user.ts
@@ -1,34 +1,47 @@
 import {checkSchema} from "express-validator";
 
 export var validateRegisterFields = () => checkSchema({
+    role: {
+        notEmpty: {
+            errorMessage: 'Role is required'
+        },
+        isIn: {
+            options: ['user', 'admin'],
+            errorMessage: 'Invalid role. Role may be user or admin'
+        }
+    },
     first_name: {
-        notEmpty: {errorMessage:'First name is required'},
+        notEmpty: {errorMessage: 'First name is required'},
         isLength: {
-            options: {min:3, max:128},
+            options: {min: 3, max: 128},
             errorMessage: 'First name must be between 3 and 128 characters long'
         }
     },
     phone: {
-        notEmpty: {errorMessage:'phone is required'},
+        notEmpty: {errorMessage: 'phone is required'},
         isLength: {
-            options: {min:8, max:32},
+            options: {min: 8, max: 32},
             errorMessage: 'phone must be between 3 and 128 characters long'
+        },
+        isMobilePhone: {
+            options: ['any'],
+            errorMessage: 'Invalid phone. Phone may be in ru-RU format'
         }
     },
-    email:{
+    email: {
         errorMessage: 'Invalid email',
-        notEmpty:{errorMessage:'Email is required'},
+        notEmpty: {errorMessage: 'Email is required'},
         isEmail: true,
         isLength: {
             options: {min: 6, max: 128},
             errorMessage: 'Email must be between 6 and 128 characters long'
         }
     },
-    password:{
+    password: {
         errorMessage: 'Invalid password',
-        notEmpty: {errorMessage:'Password is required'},
+        notEmpty: {errorMessage: 'Password is required'},
         isLength: {
-            options: {min:8, max:32},
+            options: {min: 8, max: 32},
             errorMessage: 'Password must be between 8 and 32 characters long'
         }
     }
--- src/controllers/user/register.ts
+++ src/controllers/user/register.ts
@@ -1,32 +1,32 @@
-import {Request, Response} from "express";
+import {NextFunction, Request, Response} from "express";
 import {validationResult} from "express-validator";
 import {User} from "@models/User";
-import {ValidationError} from 'sequelize'
 
-export async function register (req:Request, res:Response) {
-    try{
+export async function register(req: Request, res: Response, next: NextFunction) {
+    try {
         const result = validationResult(req)
         if (result.isEmpty()) {
+            const hasUser = await User.findOne({where:{email: req.body.email}})
+            if(hasUser) {
+                res.status(409).send({
+                    msg:'user has already been created'
+                })
+                return
+            }
+
             const user = await User.create(req.body)
             res.status(200).send({
                 msg: 'user is created: ' + user.dataValues.first_name,
-                user: {...user, password: undefined}
+                user: {...user.dataValues, password: undefined}
             })
+            return
         }
 
         res.status(400).send({
-            msg:'bad',
+            msg: 'bad',
             errors: result.array()
         })
     } catch (e) {
-        if(e instanceof ValidationError)
-            res.status(400).send({
-            msg: 'Conflict data',
-            error:e
-        })
-        res.status(500).send({
-            msg: 'Internal server error',
-            error:e
-        })
+        next(e)
     }
 }
