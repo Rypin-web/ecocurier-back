0819430219a677398f8b441950f5acf02291e766
Rypin <hhgfcswd@gmail.com>
2025-09-15 14:43:37 +0300

    feat: добавил поле для jwt

--- src/config/server.ts
+++ src/config/server.ts
@@ -2,16 +2,17 @@ import dotenv from "dotenv";
 import * as process from "node:process";
 dotenv.config()
 
-export const PORT = process.env.PORT || 5600
-export const API_URL = process.env.API_URL || 'http://localhost:3000'
-export const SALT = Number(process.env.SALT) || 13
-export const CORS = {
+export var PORT = process.env.PORT || 5600
+export var API_URL = process.env.API_URL || 'http://localhost:3000'
+export var SALT = Number(process.env.SALT) || 13
+export var SECRET_KEY = process.env.SECRET_KEY || 'secret_key'
+export var CORS = {
     origin: API_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }
 
-export const ENDPOINTS = {
+export var ENDPOINTS = {
     baseUrl: '/api',
     endpoints: {
         user: '/user',
