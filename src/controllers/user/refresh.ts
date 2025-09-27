a842f16096989d9e4c358db523096783138a5c75
Rypin <hhgfcswd@gmail.com>
2025-09-27 19:55:24 +0300

    feat: эндпоинт для обновления сессионного токена

---
+++ src/controllers/user/refresh.ts
@@ -0,0 +1,36 @@
+import {NextFunction, Request, Response} from "express";
+import {checkFingerprint, createFingerprint} from "@utils/fingerprint";
+import {ApiErrors} from "@utils/ApiErrors";
+import {Session} from "@models/Session";
+import {User} from "@models/User";
+import jwt from "jsonwebtoken";
+import {JWT_INFO} from "@config/server";
+
+export async function refresh (req:Request, res:Response, next:NextFunction) {
+    try{
+        const refreshToken = req.cookies['refresh-token'] || ''
+        const fingerprint = await createFingerprint(req)
+        if(!refreshToken || !await checkFingerprint(req, fingerprint))
+            throw ApiErrors.invalidCredentials('Invalid refresh or fingerprint. Try to login again')
+
+        const session = await Session.findOne({where:{refreshToken: refreshToken, fingerprint:fingerprint}})
+        if(!session) throw ApiErrors.invalidCredentials('Invalid refresh or fingerprint. Try to login again')
+        const user = await User.findByPk(session.userId)
+        if(!user) throw ApiErrors.userNotFound('User not found')
+
+        const sessionToken = jwt.sign(
+            {sep:user.id, role:user.role},
+            JWT_INFO.SECRET_KEY_SESSION,
+            {expiresIn: JWT_INFO.SESSION_EXPIRES_IN}
+        )
+
+        return res.status(200).send({
+            msg: 'Success refresh',
+            data: {
+                sessionToken
+            }
+        })
+    } catch (e) {
+        next(e)
+    }
+}
