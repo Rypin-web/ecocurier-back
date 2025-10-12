63a8b0fe1561922a6e7f4aea09b11262dc528140
Rypin <hhgfcswd@gmail.com>
2025-10-12 19:13:32 +0300

    feat: валидация для контроллеров категорий

---
+++ src/config/validations/categories.ts
@@ -0,0 +1,21 @@
+import {checkSchema} from "express-validator";
+
+export var validateCreateCategoryFields = () => checkSchema({
+    name: {
+        notEmpty: {errorMessage: 'Name is required'},
+        isLength: {
+            options: {min: 2, max: 255},
+            errorMessage: 'Name must be between 2 and 255 characters long'
+        },
+        trim: true,
+    },
+    description: {
+        optional: true,
+        isLength: {
+            options: {max: 10000},
+            errorMessage: 'Description is too long'
+        },
+        trim: true,
+    },
+    // imae загружается через multer: поле файла "image"; тело не должно содержать строковое поле image
+}, ['body'])
