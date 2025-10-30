07c3cb568df1cbcbba4f9f08473347e917906985
Rypin <hhgfcswd@gmail.com>
2025-10-30 18:27:43 +0300

    refactor: сделал удаления файла через промис

--- src/controllers/categories/createCategory.ts
+++ src/controllers/categories/createCategory.ts
@@ -1,6 +1,5 @@
 import {NextFunction, Request, Response} from "express";
 import {Categories} from "@models/Categories";
-import {ApiErrors} from "@utils/ApiErrors";
 import {convertToWebp} from "@utils/convertToWebp";
 import {extractBodyData} from "@utils/extractBodyData";
 
@@ -10,20 +9,15 @@ export async function createCategory(req: Request, res: Response, next: NextFunc
         const payload = extractBodyData<Categories>(req.body, ['name', 'description'])
         const file = req.file as Express.Multer.File | undefined
         const image = file ? file.filename : undefined
-
-        const exists = await Categories.findOne({where: {name: payload.name}})
-        if (exists) throw ApiErrors.alreadyExist('Category already exists')
         if (image) payload.image = image
 
         const category = await Categories.create(payload as any)
-
-        res.status(201).send({
+        return res.status(201).send({
             msg: 'category is created: ' + category.dataValues.name,
             data: {
                 category: category.dataValues
             }
         })
-        return
     } catch (e) {
         next(e)
     }
--- src/utils/convertToWebp.ts
+++ src/utils/convertToWebp.ts
@@ -2,7 +2,6 @@ import * as path from "node:path";
 import {rootPath} from "@config/server";
 import sharp from "sharp";
 import * as fs from "node:fs";
-import {ApiErrors} from "@utils/ApiErrors";
 
 export async function convertToWebp(file: Express.Multer.File | undefined) {
     if (!file) return
@@ -12,8 +11,6 @@ export async function convertToWebp(file: Express.Multer.File | undefined) {
     const wepbPath = path.resolve(rootPath, file.path.replace(filetype!, 'webp'))
 
     await sharp(imagePath).webp({quality: 80}).toFile(wepbPath)
-    fs.unlink(imagePath, (err)=> {
-        if(err) throw ApiErrors.serverError('Error deleting image', {deleteError: err})
-    })
+    await fs.promises.unlink(imagePath)
     file.filename = file.filename.replace(filetype!, 'webp')
 }
