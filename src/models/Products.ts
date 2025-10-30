07f8d63094c5f23dd65c213cf8e168d7ab3b0a1c
Rypin <hhgfcswd@gmail.com>
2025-10-30 17:35:25 +0300

    feat: модель продуктов

---
+++ src/models/Products.ts
@@ -0,0 +1,60 @@
+import {CreationOptional, DataTypes, InferAttributes, InferCreationAttributes, Model} from "sequelize";
+import {sequelize} from "@config/database";
+import {Categories} from "@models/Categories";
+
+export class Products extends Model<InferAttributes<Products>, InferCreationAttributes<Products>> {
+    declare id: CreationOptional<string>
+    declare title: string
+    declare description: CreationOptional<string>
+    declare price: number
+    declare image: CreationOptional<string>
+    declare category_id: string
+    declare createdAt: CreationOptional<Date>
+    declare updatedAt: CreationOptional<Date>
+}
+
+Products.init({
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    title: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        unique: true
+    },
+    description: {
+        type: DataTypes.TEXT,
+        defaultValue: 'Unset'
+    },
+    price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+            min: {
+                args: [0],
+                msg: 'Price must be greater than or equal to 0'
+            }
+        }
+    },
+    image: {
+        type: DataTypes.STRING(128),
+        defaultValue: 'Unset'
+    },
+    category_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+            model: Categories,
+            key: 'id'
+        }
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+}, {
+    sequelize,
+    timestamps: true,
+    modelName: 'products'
+})
