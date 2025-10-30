430c4bd9a4de5e095e85eac3be0130682dd6dd96
Rypin <hhgfcswd@gmail.com>
2025-10-30 17:35:41 +0300

    feat: связи с таблицей продуктов

--- src/utils/initAssociations.ts
+++ src/utils/initAssociations.ts
@@ -1,7 +1,11 @@
 import {Session} from "@models/Session";
 import {User} from "@models/User";
+import {Categories} from "@models/Categories";
+import {Products} from "@models/Products";
 
 export function initAssociations () {
     User.hasMany(Session, {foreignKey: 'user_id'})
     Session.belongsTo(User, {foreignKey: 'user_id'})
+    Categories.hasMany(Products, {foreignKey: 'category_id'})
+    Products.belongsTo(Categories, {foreignKey: 'category_id'})
 }
