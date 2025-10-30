ceaca5f70b3021d4f97822bc61389171def45dc9
Rypin <hhgfcswd@gmail.com>
2025-10-30 18:14:27 +0300

    refactor: добавил типизацию

--- src/utils/getUpdateData.ts
+++ src/utils/getUpdateData.ts
@@ -1,9 +1,9 @@
 import {Request} from "express";
 
-export function getUpdateData (body:Request['body'], fields:string[]) {
-    const updateData:any = {}
-    fields.forEach((e)=>{
-        if(body[e] !== undefined) updateData[e] = body[e]
+export function getUpdateData<T extends Record<string, any>>(body: Request['body'], fields: (keyof T)[]): Partial<T> {
+    const updateData: Partial<T> = {}
+    fields.forEach((e) => {
+        if (body[e] !== undefined) updateData[e] = body[e]
     })
     return updateData
 }
