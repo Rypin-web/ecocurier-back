4e2ad1fac7220c6b785d1ffe3d77ddb68e69b7ba
Rypin <hhgfcswd@gmail.com>
2025-10-31 22:13:04 +0300

    feat: подключение контроллера получения всех продуктов

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -3,12 +3,20 @@ import {ENDPOINTS} from "@config/server";
 import {requireAuthorization} from "@/middlewares/requireAuthorization";
 import {requireAdministrator} from "@/middlewares/requireAdministrator";
 import {upload} from "@config/multer";
-import {validateCreateProductFields} from "@config/validations/products";
+import {validateCreateProductFields, validateGetAllProductsFields} from "@config/validations/products";
 import {validateFields} from "@/middlewares/validateFields.middleware";
 import {createProduct} from "@controllers/products/createProduct";
+import {getAllProducts} from "@controllers/products/getAllProducts";
 
 var productsRouter = Router()
 
+productsRouter.get(
+    ENDPOINTS.products.def,
+    requireAuthorization,
+    validateGetAllProductsFields(),
+    validateFields,
+    getAllProducts
+)
 productsRouter.post(
     ENDPOINTS.products.def,
     requireAuthorization,
