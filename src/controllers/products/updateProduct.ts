a12a53ecb3d1d4c5d14fa9a27ee9b2b24be73306
Rypin <hhgfcswd@gmail.com>
2025-11-01 11:17:26 +0300

    feat: контроллер обновления продукта

---
+++ src/controllers/products/updateProduct.ts
@@ -0,0 +1,26 @@
+import {RequestWithUser} from "@/middlewares/requireAuthorization";
+import {NextFunction, Response} from "express";
+import {extractBodyData} from "@utils/extractBodyData";
+import {Products} from "@models/Products";
+import {convertToWebp} from "@utils/convertToWebp";
+import {ApiErrors} from "@utils/ApiErrors";
+
+export async function updateProduct(req: RequestWithUser, res: Response, next: NextFunction) {
+    await convertToWebp(req.file)
+
+    const payload = extractBodyData<Products>(req.body, ['title', 'price', 'description', 'category_id'])
+    if(req.file) payload.image = req.file.filename
+
+    const product = await Products.findByPk(req.params.id)
+    if(!product) throw ApiErrors.NotFound('Product not found')
+
+    await product.update(payload)
+    return res.status(200).send({
+        msg: 'Product updated successfully',
+        data: {
+            product: product.dataValues,
+            updateData: payload
+        },
+
+    })
+}
