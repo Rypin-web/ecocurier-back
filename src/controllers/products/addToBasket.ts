8e6376650b336e0dbac61cd7f7aa5f3ebb2ad2ed
Rypin <hhgfcswd@gmail.com>
2025-11-02 19:53:42 +0300

    feat: контроллер добавления продукта в корзину

---
+++ src/controllers/products/addToBasket.ts
@@ -0,0 +1,35 @@
+import { RequestWithUser } from "@/middlewares/requireAuthorization";
+import {NextFunction, Response} from "express";
+import {Products} from "@models/Products";
+import {ApiErrors} from "@utils/ApiErrors";
+import {Basket} from "@models/Basket";
+
+
+export async function addToBasket(req: RequestWithUser, res: Response, next: NextFunction) {
+    try {
+        const productId = req.params.id
+        const userId = req.user?.id
+        const quantity = Number(req.query.q)
+
+        const product = await Products.findByPk(productId)
+        if (!product) throw ApiErrors.NotFound('Product not found')
+
+        const basket = await Basket.findOne({where: {userId: userId, productId: productId}})
+        if(basket) {
+            if(basket.quantity + quantity < 1) await basket.destroy()
+            else await basket.update({quantity: basket.quantity + quantity})
+        } else {
+            await Basket.create({userId: userId!, productId: productId, quantity: Math.max(1, quantity)})
+        }
+
+        return res.status(200).send({
+            msg: 'Success add to basket',
+            data: {
+                product: product,
+                quantity: quantity
+            }
+        })
+    } catch (e) {
+        next(e)
+    }
+}
