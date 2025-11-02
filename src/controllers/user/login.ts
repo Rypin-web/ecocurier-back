933c4b5274f43ba8715a9d5b2168c647dbd144c1
Rypin <hhgfcswd@gmail.com>
2025-11-02 13:39:39 +0300

    refactor: переписал login
    - убрал фингерпринт
    - рефреш хешируется в таблице сессий
    - задается время жизни записи сессии
    - добавлено время жизни куки и отправить ее можно только с этого же домена

--- src/controllers/user/login.ts
+++ src/controllers/user/login.ts
@@ -4,38 +4,38 @@ import {User} from "@models/User";
 import bcrypt from "bcrypt";
 import jwt from 'jsonwebtoken'
 import {JWT_INFO} from "@config/server";
-import {createFingerprint} from "@utils/fingerprint";
 import {Session} from "@models/Session";
+import {hashToken} from "@utils/hashToken";
 
 export async function login(req: Request, res: Response, next: NextFunction) {
     try {
         const user = await User.findOne({where: {email: req.body.email}})
         if (!user) throw ApiErrors.NotFound('User not found')
         const isPasswordValid = await bcrypt.compare(req.body.password, user.dataValues.password)
-        if (!isPasswordValid) throw ApiErrors.invalidCredentials('Invalid credentials')
+        if (!isPasswordValid) throw ApiErrors.invalidCredentials('Invalid data')
 
-        const fingerprint = await createFingerprint(req)
         const sessionToken = jwt.sign(
-            {sep: user.id, role: user.role},
+            {id: user.id, role: user.role},
             JWT_INFO.SECRET_KEY_SESSION,
             {expiresIn: JWT_INFO.SESSION_EXPIRES_IN}
         )
         const refreshToken = jwt.sign(
-            {...user.dataValues, password: undefined},
+            {id: user.id},
             JWT_INFO.SECRET_KEY_REFRESH,
             {expiresIn: JWT_INFO.REFRESH_EXPIRES_IN}
         )
         await Session.create({
             userId: user.id,
-            refreshToken: refreshToken,
-            fingerprint: fingerprint
+            refreshToken: hashToken(refreshToken),
+            expiresAt: new Date(Date.now() + JWT_INFO.REFRESH_EXPIRES_IN),
         })
-
         res.cookie('refresh-token', refreshToken, {
             httpOnly: true,
+            sameSite: 'strict',
+            maxAge: JWT_INFO.REFRESH_EXPIRES_IN,
         })
         return res.status(200).send({
-            msg: 'Success login. Hello ' + user.dataValues.first_name,
+            msg: 'Success login',
             data: {
                 user: {...user.dataValues, password: undefined},
                 sessionToken,
