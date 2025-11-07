02f72bb8b569bdab99301112c79b72e329022c43
Rypin <hhgfcswd@gmail.com>
2025-11-07 22:18:03 +0300

    feat: модель заказа

---
+++ src/models/Order.ts
@@ -0,0 +1,50 @@
+import {CreationOptional, DataTypes, InferAttributes, InferCreationAttributes, Model} from "sequelize";
+import {sequelize} from "@config/database";
+import {User} from "@models/User";
+
+export class Order extends Model<InferAttributes<Order>, InferCreationAttributes<Order>> {
+    declare id: CreationOptional<string>
+    declare userId: string
+    declare status: string
+    declare totalPrice: number
+    declare deliveryAddress: string
+    declare createdAt: CreationOptional<Date>
+    declare updatedAt: CreationOptional<Date>
+}
+
+Order.init({
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+    },
+    status: {
+        type: DataTypes.ENUM('pending', 'shipped', 'delivered', 'cancelled'),
+        allowNull: false,
+        defaultValue: 'pending'
+    },
+    totalPrice: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    deliveryAddress: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+}, {
+    sequelize,
+    timestamps: true,
+    tableName: 'orders'
+})
