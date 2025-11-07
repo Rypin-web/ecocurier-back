dc1257e08650d4dfbb4ebdc1bf54a9a9785a003b
Rypin <hhgfcswd@gmail.com>
2025-11-07 22:59:59 +0300

    feat: ассоциации для новых моделей Order и OrderItem

--- src/utils/initAssociations.ts
+++ src/utils/initAssociations.ts
@@ -3,6 +3,8 @@ import {User} from "@models/User";
 import {Categories} from "@models/Categories";
 import {Products} from "@models/Products";
 import {Basket} from "@models/Basket";
+import {Order} from "@models/Order";
+import {OrderItem} from "@models/OrderItem";
 
 export function initAssociations () {
     User.hasMany(Session, {foreignKey: 'user_id'})
@@ -15,4 +17,15 @@ export function initAssociations () {
     Products.hasMany(Basket, {foreignKey: 'product_id'})
     Basket.belongsTo(User, {foreignKey: 'user_id'})
     Basket.belongsTo(Products, {foreignKey: 'product_id'})
+
+    User.hasMany(Order, {foreignKey: 'userId', as: 'orders'})
+    User.hasOne(Order, {foreignKey: 'courierId', as: 'deliveries'})
+    Order.belongsTo(User, {foreignKey: 'userId', as: 'customer'})
+    Order.belongsTo(User, {foreignKey: 'courierId', as: 'courier'})
+
+    Order.hasMany(OrderItem, {foreignKey: 'orderId', as: 'items'})
+    OrderItem.belongsTo(Order, {foreignKey: 'orderId', as: 'order'})
+
+    Products.hasMany(OrderItem, {foreignKey: 'productId', as: 'orderItems'})
+    OrderItem.belongsTo(Products, {foreignKey: 'productId', as: 'product'})
 }
