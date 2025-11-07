9453f3f66e1d46f63f50b30abf4004eb3f224a01
Rypin <hhgfcswd@gmail.com>
2025-11-07 23:07:03 +0300

    fix: не было переменной imagePath

--- src/utils/deleteImage.ts
+++ src/utils/deleteImage.ts
@@ -1,6 +1,11 @@
 import * as fs from 'fs';
+import * as path from "node:path";
+import {rootPath} from "@config/server";
 
 export async function deleteImage (file: Express.Multer.File | undefined) {
     if(!file) return
+    const imagePath = path.isAbsolute(file.path)
+        ? file.path
+        : path.join(rootPath, file.path)
     await fs.promises.unlink(imagePath)
 }
