df3b1ce7c486c0d6245a49e5932d640934c6cda6
Rypin <hhgfcswd@gmail.com>
2025-11-09 20:08:29 +0300

    feat: контроллер формирования заказа

---
+++ src/controllers/user/makeOrder.ts
@@ -0,0 +1,59 @@
+import {RequestWithUser} from "@/middlewares/requireRole";
+import {NextFunction, Response} from "express";
+import {extractBodyData} from "@utils/extractBodyData";
+import {Order} from "@models/Order";
+import {Basket} from "@models/Basket";
+import {Products} from "@models/Products";
+import {ApiErrors} from "@utils/ApiErrors";
+import {sequelize} from "@config/database";
+import {OrderItem} from "@models/OrderItem";
+
+export async function makeOrder(req: RequestWithUser, res: Response, next: NextFunction) {
+    try {
+        const result = await sequelize.transaction(async (t) => {
+            const orderPayload = extractBodyData<Order>(req.body, ['deliveryAddress'])
+            orderPayload.userId = req.user?.id
+            orderPayload.status = 'pending'
+
+            const userBasket = await Basket.findAll({
+                where: {
+                    userId: req.user?.id
+                },
+                include: {
+                    model: Products,
+                    attributes: ['id', 'title', 'image', 'price', 'description']
+                },
+                attributes: ['id', 'quantity'],
+                transaction: t
+            }) as (Basket & { product: Products })[]
+
+            if (userBasket.length < 1) throw ApiErrors.invalidCredentials('Basket is empty')
+
+            orderPayload.totalPrice = userBasket.reduce(
+                (pv, cv) => pv + cv.quantity * cv.product.price,
+                0
+            )
+
+            const order = await Order.create(orderPayload as any, {transaction: t})
+            const orderItems = await Promise.all(userBasket.map(async (item) =>
+                await OrderItem.create({
+                    orderId: order.id,
+                    productId: item.product.id,
+                    quantity: item.quantity
+                }, {transaction: t})
+            ))
+            await Basket.destroy({where: {userId: req.user?.id}, transaction: t})
+
+            return {order, orderItems}
+        })
+        return res.status(201).send({
+            msg: 'Success make order',
+            data: {
+                order: result.order,
+                orderItems: result.orderItems
+            }
+        })
+    } catch (e) {
+        next(e)
+    }
+}
