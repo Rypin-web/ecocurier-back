612d2b32018642d37da6963f77a0347f4865460d
Rypin <hhgfcswd@gmail.com>
2025-11-09 20:59:33 +0300

    refactor: еще один контроллер теперь с логикой транзакций

--- src/controllers/user/updateUser.ts
+++ src/controllers/user/updateUser.ts
@@ -2,24 +2,28 @@ import {NextFunction, Request, Response} from "express";
 import {ApiErrors} from "@utils/ApiErrors";
 import {User} from "@models/User";
 import {extractBodyData} from "@utils/extractBodyData";
+import {sequelize} from "@config/database";
 
 export async function updateUser(req: Request, res: Response, next: NextFunction) {
     try {
-        const userId = req.params.id
-        if (!userId) throw ApiErrors.invalidCredentials('suerId is required')
-        const user = await User.findByPk(userId)
-        if (!user) throw ApiErrors.NotFound('Not found')
+        const result = await sequelize.transaction(async (t) => {
+            const userId = req.params.id
+            if (!userId) throw ApiErrors.invalidCredentials('userId is required')
+            const user = await User.findByPk(userId, {transaction: t})
+            if (!user) throw ApiErrors.NotFound('Not found')
 
-        let payload = extractBodyData<User>(req.body, ['role', 'first_name', 'last_name', 'email', 'phone'])
-        await user.update(payload)
+            let payload = extractBodyData<User>(req.body, ['role', 'first_name', 'last_name', 'email', 'phone'])
+            await user.update(payload, {transaction: t})
+            return {user, payload}
+        })
         return res.status(201).send({
             msg: 'success update',
             data: {
                 user: {
-                    ...user.dataValues,
+                    ...result.user.dataValues,
                     password: undefined
                 },
-                updatedData: payload
+                updatedData: result.payload
             }
         })
     } catch (e) {
