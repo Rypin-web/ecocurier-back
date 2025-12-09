5a90338dd33348381f839175f447121a059a112a
Rypin <hhgfcswd@gmail.com>
2025-12-09 17:23:05 +0300

    feat: сортировка для таблички пользователей

--- src/config/server.ts
+++ src/config/server.ts
@@ -21,7 +21,6 @@ export var CORS = {
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    optionsSuccessStatus: 204
 }
 
 export var ENDPOINTS = {
--- src/controllers/user/getAllUsers.ts
+++ src/controllers/user/getAllUsers.ts
@@ -1,16 +1,26 @@
 import {NextFunction, Request, Response} from "express";
 import {User} from "@models/User";
 
+async function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
 export async function getAllUsers(req: Request, res: Response, next: NextFunction) {
     try {
+        const sort = req.query.sort as 'ASC' | 'DESC' || 'ASC'
+        const sortBy = req.query.sortBy as string || 'createdAt'
         const page = Number(req.query.page) || 1
         const limit = Math.min(Number(req.query.limit) || 10, 100)
         const offset = (page - 1) * limit
         const users = await User.findAndCountAll({
             offset: offset,
             limit: limit,
+            order: [[sortBy, sort]],
             attributes: {exclude: ['password']}
         })
+
+        await sleep(1000)
+
         return res.status(200).send({
             msg: 'Success get users',
             data: {
